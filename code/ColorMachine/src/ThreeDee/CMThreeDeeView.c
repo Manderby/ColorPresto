0cf364197f541ac0cc150393c36a8d17035bdb31
Tobias Stamm <manderby@manderby.com>
2022-11-19 15:27:36 +0100

    UVW improvements

--- code/ColorMachine/src/ThreeDee/CMThreeDeeView.c
+++ code/ColorMachine/src/ThreeDee/CMThreeDeeView.c
@@ -341,12 +341,12 @@ void cmDrawThreeDeeSurfaces(const CMLColorMachine* cm, const CMLColorMachine* sm
       break;
     case CML_COLOR_UVW:
       cmlSet4UInt(surfaceSteps[0], steps3D, steps3D, 1, 1);
-      cmlSet4(origins[0], 0.f, 0.f, 0.f, 0.f);
+      cmlSet4(origins[0], 0.f, 0.f, .3f, 0.f);
       cmlSet4(axis1s[0], 0.f, 1.f, 0.f, 0.f);
-      cmlSet4(axis2s[0], 0.f, 0.f, 1.f, 0.f);
+      cmlSet4(axis2s[0], 0.f, 0.f, .7f, 0.f);
       cmlSet4UInt(surfaceSteps[1], steps3D, steps3D, 1, 1);
-      cmlSet4(origins[1], 0.f, 0.f, 0.f, 0.f);
-      cmlSet4(axis1s[1], 0.f, 0.f, 1.f, 0.f);
+      cmlSet4(origins[1], 0.f, 0.f, .3f, 0.f);
+      cmlSet4(axis1s[1], 0.f, 0.f, .7f, 0.f);
       cmlSet4(axis2s[1], 1.f, 0.f, 0.f, 0.f);
       cmlSet4UInt(surfaceSteps[2], steps3D, steps3D, 1, 1);
       cmlSet4(origins[2], 1.f, 1.f, 1.f, 1.f);
@@ -355,7 +355,7 @@ void cmDrawThreeDeeSurfaces(const CMLColorMachine* cm, const CMLColorMachine* sm
       cmlSet4UInt(surfaceSteps[3], steps3D, steps3D, 1, 1);
       cmlSet4(origins[3], 1.f, 1.f, 1.f, 1.f);
       cmlSet4(axis1s[3], 0.f, -1.f, 0.f, 0.f);
-      cmlSet4(axis2s[3], 0.f, 0.f, -1.f, 0.f);
+      cmlSet4(axis2s[3], 0.f, 0.f, -.7f, 0.f);
       cmlSet4UInt(surfaceSteps[4], steps3D, steps3D, 1, 1);
       cmlSet4(origins[4], 1.f, 1.f, 1.f, 1.f);
       cmlSet4(axis1s[4], 0.f, 0.f, -1.f, 0.f);
