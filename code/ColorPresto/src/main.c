76186d29e2248fca33e0270a8d61adcba0869c88
Tobias Stamm <manderby@manderby.com>
2025-04-22 15:35:36 +0200

    Fixing some minor bugs

--- code/ColorPresto/src/main.c
+++ code/ColorPresto/src/main.c
@@ -93,7 +93,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
   NA_UNUSED(lpCmdLine);
   NA_UNUSED(nShowCmd);
   
-  naOpenConsoleWindow();
+  //naOpenConsoleWindow();
 
   naStartRuntime();
   naStartApplication(preStartup, postStartup, NA_NULL, NA_NULL);
